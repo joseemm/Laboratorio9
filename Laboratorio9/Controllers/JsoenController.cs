24fd3c54a0c9782467cb6f5524ff481a2077521f
joseemm <66448940+joseemm@users.noreply.github.com>
2022-09-27 00:02:04 -0400

    Add project files.

---
+++ Laboratorio9/Controllers/JsoenController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio9.Models;
+
+namespace Laboratorio9.Controllers
+{
+    public class JsoenController : Controller
+    {
+        // GET: Jsoen
+        public ActionResult Index()
+        {
+
+            
+            return View();
+        }
+
+        public JsonResult ObtenerClientes()
+        {
+
+            List<Cliente> lst;
+            using (ClientesEntities db = new ClientesEntities())
+            {
+                lst = (from d in db.Clientes
+                       select d).ToList();
+
+
+            }
+
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Laboratorio9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboratorio9.MvcApplication" Language="C#" %>
